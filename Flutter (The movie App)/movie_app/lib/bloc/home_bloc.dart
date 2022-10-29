a84acab7739ac013247a309358dc094b16e26e99
Saw-YanLinOo <yly80103@gmail.com>
2022-10-30 04:34:08 +0630

    📝 Getting necessary from bloc and assigning them to state variables

--- Flutter (The movie App)/movie_app/lib/bloc/home_bloc.dart
+++ Flutter (The movie App)/movie_app/lib/bloc/home_bloc.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/foundation.dart';
+import 'package:movie_app/data/models/movie_model_impl.dart';
 import 'package:movie_app/data/vos/actor_vo.dart';
 import 'package:movie_app/data/vos/genre_vo.dart';
 import 'package:movie_app/data/vos/movie_vo.dart';
+
+import '../data/models/movie_model.dart';
 
 class HomeBloc extends ChangeNotifier {
   //State
@@ -11,4 +14,68 @@ class HomeBloc extends ChangeNotifier {
   List<ActorVO>? mActors;
   List<MovieVO>? mShowCaseMoveList;
   List<MovieVO>? mMovieByGenreList;
+
+  //Model
+  MovieModel mMovieModel = MovieModelImpl();
+
+  HomeBloc() {
+    /// Now Playing Movies from database
+    mMovieModel.getNowPlayingMoviesFromDatabase().listen((movieList) {
+      mNowPlayingMovieList = movieList ?? [];
+      notifyListeners();
+    }).onError((error) {});
+
+    /// Popular Movies from database
+    mMovieModel.getPopularMoviesFromDatabase().listen((movieList) {
+      mPopularMoviesList = movieList ?? [];
+      notifyListeners();
+    }).onError((error) {});
+
+    /// Genres
+    mMovieModel.getGenres().then((genreList) {
+      mGenerList = genreList ?? [];
+      notifyListeners();
+
+      /// Movies By Genres
+      _getMoviesByGenreAndRefresh(mGenerList?.first.id ?? 0);
+    }).catchError((error) {
+      debugPrint('error:::::: $error');
+    });
+
+    /// Genres from database
+    mMovieModel.getGenresFromDatabase().then((genreList) {
+      mGenerList = genreList ?? [];
+      notifyListeners();
+
+      /// Movies By Genres
+      _getMoviesByGenreAndRefresh(mGenerList?.first.id ?? 0);
+    }).catchError((error) {
+      debugPrint('error:::::: $error');
+    });
+
+    /// Showcase from database
+    mMovieModel.getTopRatedMoviesFromDatabase().listen((movieList) {
+      mShowCaseMoveList = movieList ?? [];
+      notifyListeners();
+    }).onError((error) {});
+
+    /// Actors
+    mMovieModel.getActors(1).then((actorList) {
+      mActors = actorList ?? [];
+      notifyListeners();
+    }).catchError((error) {});
+
+    /// Actors from database
+    mMovieModel.getActorsFromDatabase().then((actorList) {
+      mActors = actorList ?? [];
+      notifyListeners();
+    }).catchError((error) {});
+  }
+
+  _getMoviesByGenreAndRefresh(int generalId) {
+    mMovieModel.getMoviesByGenre(generalId).then((movieList) {
+      mMovieByGenreList = movieList ?? [];
+      notifyListeners();
+    }).catchError((error) {});
+  }
 }
